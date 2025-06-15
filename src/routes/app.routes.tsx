e49445e4d3d6ea23b9a37dfb9af4e1d9fb09a986
Lucas Cid <lucasfelipaaa@gmail.com>
2025-06-15 19:01:35 -0300

    refactor: remove Flow component and update routing for FlowModels and FlowInstances

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,18 +1,20 @@
 import { AppLayout } from "@/layout/app";
 import { Approvals } from "@/pages/app/Approvals";
 import { Dashboard } from "@/pages/app/Dashboard";
-import { Flow } from "@/pages/app/Flow";
+import { FlowModels } from "@/pages/app/FlowModels";
 import { FlowDiagram } from "@/pages/app/FlowDiagram";
 import { Forms } from "@/pages/app/Forms";
 import { Route, Routes } from "react-router-dom";
+import { FlowInstances } from "@/pages/app/FlowInstances";
 
 export function AppRoutes () {
   return (
     <Routes>
       <Route path="/" element={<AppLayout />}>
         <Route path="/dashboard" element={<Dashboard />}/>
-        <Route path="/flows" element={<Flow />}/>
-        <Route path="/flows/diagram/:id" element={<FlowDiagram />}/>
+        <Route path="/flows/models/" element={<FlowModels />}/>
+        <Route path="/flows/instances" element={<FlowInstances />}/>
+        <Route path="/flows/diagram/:type/:id" element={<FlowDiagram />}/>
         <Route path="/forms" element={<Forms />}/>
         <Route path="/Approvals" element={<Approvals />}/>
       </Route>
--- src/routes/index.routes.tsx
+++ src/routes/index.routes.tsx
@@ -3,8 +3,17 @@ import { BrowserRouter } from "react-router-dom"
 import { AppRoutes } from "./app.routes";
 import { AuthRoutes } from "./auth.routes";
 import { Toaster } from "@/components/ui/sonner";
+import { useEffect } from "react";
+import { useThemeStore } from "@/store/theme";
 export function Router() {
   const user = useUserStore(state => state.user);
+  const theme = useThemeStore(state => state.theme);
+  useEffect(() => {
+    const html = document.getElementsByTagName("html")[0];
+    if (html) {
+      html.classList.add(theme ?? "light");
+    }
+  },[theme])
   return (
     <BrowserRouter>
       <Toaster />
