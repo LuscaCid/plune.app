7d8b114b739fbffc852fadc013213328939bde1f
Lucas Cid <lucasfelipaaa@gmail.com>
2025-06-16 14:14:02 -0300

    feat: replace anchor tags with Link components for client-side navigation in AppSidebar and NavMain

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -13,17 +13,21 @@ import { TypographyH4 } from "./ui/Typography";
 import { UserDropdown } from "./UserDropdown";
 import { footerItems, headerItems } from "@/lib/routes";
 import { NavMain } from "./ui/NavMain";
+import { Link } from "react-router-dom";
 export function AppSidebar() {
 
   return (
     <Sidebar variant="floating" className="p-2 border-zinc-200 dark:border-zinc-800/90 " >
       <SidebarHeader className="border-b border-b-zinc-300 dark:border-b-zinc-800">
-        <a title="/Dashboard" href="/">
-          <div className="px-2 py-1 cursor-pointer flex flex-row items-center gap-1 w-full rounded-md hover:bg-zinc-100 dark:hover:bg-zinc-800 transition duration-150 p-0">
-            <ChartBar size={20} />
-            <TypographyH4 content="Plune.app" />
-          </div>
-        </a>
+        <SidebarMenuButton>
+          <Link to={"/"}>
+            <div className="px-2 py-1 cursor-pointer flex flex-row items-center gap-1 w-full rounded-md hover:bg-zinc-100 dark:hover:bg-zinc-800 transition duration-150 p-0">
+              <ChartBar size={20} />
+              <TypographyH4 content="Plune.app" />
+            </div>
+          </Link>
+        </SidebarMenuButton>
+
       </SidebarHeader>
       <SidebarContent className="p-2 flex flex-col justify-between dark:bg-zinc-900" >
         <SidebarGroupContent>
@@ -31,10 +35,10 @@ export function AppSidebar() {
             {headerItems.map((item) => (
               <SidebarMenuItem key={item.url + item.title}>
                 <SidebarMenuButton className="flex gap-2 items-center" asChild>
-                  <a href={item.url}>
+                  <Link to={item.url}>
                     <item.icon size={20} />
                     {item.title}
-                  </a>
+                  </Link>
                 </SidebarMenuButton>
               </SidebarMenuItem>
             ))}
@@ -49,8 +53,8 @@ export function AppSidebar() {
                     url: "/flows/models"
                   },
                   {
-                    title : "Instances",
-                    url : "/flows/instances"
+                    title: "Instances",
+                    url: "/flows/instances"
                   }
                 ]
               }
