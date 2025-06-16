6cee691d2bb6ad749a85b663d1f6761b08df5fdb
Lucas Cid <lucasfelipaaa@gmail.com>
2025-06-15 22:52:51 -0300

    feat: add DefaultNodeComponents for handling node connections in React Flow

---
+++ src/components/ReactFlow/DefaultNodeComponents.tsx
@@ -0,0 +1,21 @@
+import { Handle, Position, type NodeProps } from "reactflow";
+interface Props {
+  nodeProps: NodeProps
+}
+export function DefaultNodeComponents({ nodeProps }: Props) {
+  return (
+    <>
+      <Handle
+        id="right"
+        type="source"
+        position={Position.Right}
+        className={`${!nodeProps.selected && "opacity-90"} p-1 bg-zinc-200 dark:bg-zinc-200 -mr-5`}
+      />
+      <Handle
+        className={`${!nodeProps.selected && "opacity-90"} p-1 bg-zinc-200 dark:bg-zinc-200 -ml-5`}
+        id="left"
+        type="source"
+        position={Position.Left}
+      /></>
+  );
+}
