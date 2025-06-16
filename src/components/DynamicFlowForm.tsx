6c44c9715616dff94d8bae9c334441387979894d
Lucas Cid <lucasfelipaaa@gmail.com>
2025-06-16 14:13:45 -0300

    feat: implement DynamicFlowForm component with drag-and-drop field reordering

---
+++ src/components/DynamicFlowForm.tsx
@@ -0,0 +1,97 @@
+import type { Form, FormField as FormFieldType } from "@/@types/Form";
+import { useUserStore } from "@/store/user";
+import { Reorder, useDragControls, useMotionValue } from "framer-motion";
+import { useCallback, useState } from "react";
+import { FormProvider, useForm } from "react-hook-form";
+import { Button } from "./ui/button";
+import { FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "./ui/form";
+import { Input } from "./ui/input";
+import { Grip } from "lucide-react";
+interface Props {
+  form?: Form;
+  isEditable?: boolean;
+}
+export function DynamicFlowForm({ form, isEditable }: Props) {
+  const user = useUserStore(state => state.user);
+  const methods = useForm();
+  const y = useMotionValue(0);
+  const dragControls = useDragControls();
+
+  const [fieldsOrder, setFieldsOrder] = useState<FormFieldType[]>(form ? form.fields : []);
+  const [fieldsIds, setFieldsIds] = useState(
+    form && form.fields ? form.fields.map((field) => field.name) : []
+  );
+
+  const onReorderFields = useCallback((newOrder: (number | string)[]) => {
+    const newFieldsOrder = newOrder
+      .map(
+        (order) => fieldsOrder
+          .find(p => p.name == order)!);
+
+    setFieldsOrder(newFieldsOrder.map((_, i) => ({ ..._, order: i })));
+  }, [fieldsOrder]);
+
+  const handleSubmit = useCallback(() => {
+
+  }, [])
+  return (
+    <>
+      {form && (
+        <FormProvider {...methods}>
+          <form className=" flex flex-col gap-6" onSubmit={methods.handleSubmit(handleSubmit)}>
+            <Reorder.Group
+              onReorder={onReorderFields}
+              values={fieldsIds}
+              className="flex flex-col gap-4 overflow-hidden"
+            >
+              {fieldsOrder.map((order) => (
+                <Reorder.Item
+                  dragListener={false}
+                  value={order.name}
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                  style={{ y }}
+                  key={order.name}
+                  dragControls={dragControls}
+                  className={"relative flex w-full gap-2"}
+                >
+                  <Button
+                    variant={"ghost"}
+                    className={""}
+                    onPointerDown={(event) => dragControls.start(event)}
+                    type="button"
+                  >
+                    <Grip />
+                  </Button>
+                  <FormField
+                    control={methods.control}
+                    name={order.name}
+                    render={({ field }) => (
+                      < FormItem className="p-2 rounded-lg border border-dashed w-full">
+                        <FormLabel>{order.label}</FormLabel>
+                        <FormControl>
+                          <Input
+                            required={order.required}
+                            placeholder={order.label}
+                            {...field}
+                            defaultValue={order.value ?? ""}
+                          />
+                        </FormControl>
+                        <FormDescription>This is your public display name.</FormDescription>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </Reorder.Item>
+              ))}
+            </Reorder.Group>
+            <Button type="submit" variant={"outline"}>
+              Submit
+            </Button>
+          </form>
+        </FormProvider>
+      )}
+    </>
+  );
+}
--- src/components/ReactFlow/FormNodeType.tsx
+++ src/components/ReactFlow/FormNodeType.tsx
@@ -1,79 +1,17 @@
-import { NodeToolbar, Position, useReactFlow, type NodeProps } from "reactflow";
-import React, { useCallback, useState } from "react";
+import { NodeToolbar, Position, type NodeProps } from "reactflow";
+import React, { useCallback } from "react";
 import "@reactflow/node-resizer/dist/style.css"
 import { FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "../ui/form";
 import { Input } from "../ui/input";
 import { FormProvider, useForm } from "react-hook-form";
-import type { FlownNodeData, FlowNode } from "@/@types/Flow";
+import type { FlownNodeData } from "@/@types/Flow";
 import { Button } from "../ui/button";
 import { DefaultNodeComponents } from "./DefaultNodeComponents";
 import { FlowCard } from "../ui/FlowCard";
 import { Trash } from "lucide-react";
-import { useDragField } from "@/store/use-drag-field";
-import { useMotionValue, Reorder } from "framer-motion";
 
 export const FormNodeType = React.memo((nodeProps: NodeProps<FlownNodeData>) => {
   const methods = useForm();
-  const { setNodes, getNodes } = useReactFlow();
-  const y = useMotionValue(0);
-  const setIsDragging = useDragField((state) => state.setIsDraggingAnyField);
-  const [fieldsOrder, setFieldsOrder] = useState<string[]>(() => {
-    if (nodeProps.data.form && nodeProps.data.form.fields.length > 0) {
-      return nodeProps.data.form.fields.map((field) => field.name);
-    }
-    return []
-  });
-
-  const handleDragColumn = useCallback(() => {
-    setNodes(
-      getNodes().map((nd) => {
-        if (nodeProps.id == nd.id) {
-          setIsDragging(true);
-          return { ...nd, draggable: false }
-        }
-        return nd;
-      })
-    );
-  }, [setNodes, getNodes, nodeProps, setIsDragging]);
-
-  const dragCancel = useCallback(() => {
-    setNodes(
-      getNodes().map((nd) => {
-        if (nodeProps.id == nd.id) {
-          setIsDragging(false);
-          return { ...nd, draggable: true }
-        }
-        return nd;
-      })
-    );
-  }, [setNodes, getNodes, nodeProps, setIsDragging])
-
-  const onReorderNodeCols = async (newOrder: string[]) => {
-    //rearranjo do array de colunas dentro da tabela de acordo com a ordenacao retornada no argumento da funcao
-    setFieldsOrder(newOrder);
-    if (nodeProps.data.form) {
-      const newOrderForNode = newOrder
-        .map((order: string) => nodeProps.data.form!.fields
-          .find((field) => field.name == order)!);
-
-      setNodes(
-        (getNodes() as FlowNode[]).map(nd => {
-          if (nd.id === nodeProps.id) {
-            return {
-              ...nd,
-              data: {
-                ...nd.data,
-                form: { fields: newOrderForNode }
-              }
-            };
-          }
-          return nd;
-        })
-      );
-      // await updateNode(nodeProps);
-    }
-
-  }
 
   const handleSubmit = useCallback((data: unknown) => {
     console.log(data);
@@ -86,56 +24,30 @@ export const FormNodeType = React.memo((nodeProps: NodeProps<FlownNodeData>) => 
       {nodeProps.data.form && (
         <FormProvider {...methods}>
           <form className="flex flex-col gap-4" onSubmit={methods.handleSubmit(handleSubmit)}>
-            <Reorder.Group
-              axis="y"
-              onReorder={onReorderNodeCols}
-              values={fieldsOrder}
-              className="w-full overflow-hidden transition-none "
 
-            >
-              {fieldsOrder.map((order) => {
-                const formField = nodeProps.data.form!.fields.find((field) => field.name == order)
-                if (formField) {
-                  return (
-                    <Reorder.Item
-                      onPointerDown={handleDragColumn}
-                      onPointerUp={dragCancel}
-                      onPointerOut={dragCancel}
-                      value={formField.name}
-                      initial={{ opacity: 0 }}
-                      animate={{ opacity: 1 }}
-                      exit={{ opacity: 0 }}
-                      style={{ y }}
-                      key={formField.name}
-                    >
-                      <FormField
-                        control={methods.control}
-                        name={formField.name}
-                        render={({ field }) => (
-                          < FormItem className="p-2 rounded-lg border border-dashed">
+            { nodeProps.data.form.fields.map((formField) => (
+              <FormField
+                key={formField.name}
+                control={methods.control}
+                name={formField.name}
+                render={({ field }) => (
+                  < FormItem className="p-2 rounded-lg border border-dashed">
 
-                            <FormLabel>{formField.label}</FormLabel>
-                            <FormControl>
-                              <Input
-                                required={formField.required}
-                                placeholder={formField.label}
-                                {...field}
-                                defaultValue={formField.value ?? ""}
-                              />
-                            </FormControl>
-                            <FormDescription>This is your public display name.</FormDescription>
-                            <FormMessage />
-                          </FormItem>
-                        )}
+                    <FormLabel>{formField.label}</FormLabel>
+                    <FormControl>
+                      <Input
+                        required={formField.required}
+                        placeholder={formField.label}
+                        {...field}
+                        defaultValue={formField.value ?? ""}
                       />
-                    </Reorder.Item>
-                  )
-
-                }
-              })
-            }
-            </Reorder.Group>
-
+                    </FormControl>
+                    <FormDescription>This is your public display name.</FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            ))}
             <Button type="submit" variant={"outline"}>
               Submit
             </Button>
