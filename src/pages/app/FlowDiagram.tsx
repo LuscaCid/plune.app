23957e627e35b0ab3ebf2fad7ee6cb8c0bbb09bb
Lucas Cid <lucasfelipaaa@gmail.com>
2025-06-18 22:43:35 -0300

    feat: enhance FlowDiagram component with edge types, connection mode, and UI panel for node actions

--- src/pages/app/FlowDiagram.tsx
+++ src/pages/app/FlowDiagram.tsx
@@ -1,4 +1,4 @@
-import ReactFlow, { useNodesState, useEdgesState, Controls, Background, MiniMap, BackgroundVariant, addEdge, type Connection } from "reactflow"
+import ReactFlow, { useNodesState, useEdgesState, Controls, Background, MiniMap, BackgroundVariant, type Connection, ConnectionMode, MarkerType, Panel } from "reactflow"
 import type { Flow } from "@/@types/Flow";
 import { FloatingEdge } from "@/components/ReactFlow/FloatingEdge";
 import { FormNodeType } from "@/components/ReactFlow/FormNodeType";
@@ -6,22 +6,28 @@ import { StageNodeType } from "@/components/ReactFlow/StageNodeType";
 import { useQueryClient } from "@tanstack/react-query";
 import { useCallback, useEffect, useMemo } from "react";
 import { useParams } from "react-router-dom";
-import 'reactflow/dist/style.css';
 import { ConditionNodeType } from "@/components/ReactFlow/ConditionNodeType";
 import { ApprovalNodeType } from "@/components/ReactFlow/ApprovalNodeType";
 import { WebhookNodeType } from "@/components/ReactFlow/WebhookNodeType";
 import { useDragField } from "@/store/use-drag-field";
+import { Button } from "@/components/ui/button";
+import { FormInput } from "lucide-react";
+import { DropdownMenuSeparator } from "@/components/ui/dropdown-menu";
+
 
 export function FlowDiagram() {
   const nodeTypes = useMemo(() => ({
     form: FormNodeType,
-    end: StageNodeType,
-    start: StageNodeType,
+    stage: StageNodeType,
     approval: ApprovalNodeType,
     condition: ConditionNodeType,
     webhook: WebhookNodeType
   }), []);
+  const EDGE_TYPES = useMemo(() => ({
+    "floating": FloatingEdge
+  }), []);
   const isDraggingAnyField = useDragField((state) => state.isDraggingAnyField);
+
 
   const queryClient = useQueryClient();
   const params = useParams();
@@ -35,12 +41,8 @@ export function FlowDiagram() {
       return flowsDictionary[params.type];
     }
   }, [params]);
-
   const [nodes, setNodes, onNodesChange] = useNodesState([]);
   const [edges, setEdges, onEdgesChange] = useEdgesState([]);
-  const EDGE_TYPES = useMemo(() => ({
-    "floating": FloatingEdge
-  }), []);
 
   useEffect(() => {
     const orgFlows = queryClient.getQueryData(queryKeyBytype ?? []) as Flow[] ?? [];
@@ -54,7 +56,6 @@ export function FlowDiagram() {
             const orgFlowsRefetched = queryClient.getQueryData(queryKeyBytype ?? []) as Flow[] ?? [];
 
             const flowFoundByRefetch = orgFlowsRefetched.find((instance) => instance.id === params.id);
-            console.log(flowFoundByRefetch);
             if (flowFoundByRefetch) setNodes(flowFoundByRefetch.nodes);
 
           })
@@ -63,10 +64,22 @@ export function FlowDiagram() {
     }
   }, [params, queryClient, setNodes, queryKeyBytype]);
 
-  const onConnect = useCallback(
-    (connection: Connection) => setEdges((eds) => addEdge(connection, eds)),
-    [setEdges],
-  );
+  const onConnect = useCallback((connection: Connection) => {
+    if (connection.source && connection.target) {
+      setEdges(
+        [...edges, {
+          id: connection.source! + connection.target + Math.round((Math.random() * 10000)),
+          source: connection.source,
+          target: connection.target,
+          animated: true,
+          type: "floating",
+          markerEnd: { type: MarkerType.Arrow },
+        }]
+      )
+    }
+    // queryClient.setQueryData()
+    //salvar isso em cache
+  }, [setEdges, edges]);
 
   return (
     <div className="w-full h-full">
@@ -74,16 +87,41 @@ export function FlowDiagram() {
         minZoom={0.1}
         maxZoom={2}
         onConnect={onConnect}
+        edgeTypes={EDGE_TYPES}
+        nodeTypes={nodeTypes}
         proOptions={{ hideAttribution: true }}
         nodes={nodes}
         edges={edges}
+        defaultEdgeOptions={{
+          type: "floating",
+          animated: true,
+          markerEnd: { type: MarkerType.Arrow }
+        }}
+        // connectionLineComponent={FloatingConnectionLine}
+        connectionMode={ConnectionMode.Loose}
         onEdgesChange={onEdgesChange}
         onNodesChange={onNodesChange}
         fitView
         panOnDrag={!isDraggingAnyField}
-        edgeTypes={EDGE_TYPES}
-        nodeTypes={nodeTypes}
       >
+        <Panel position="top-right" className="border border-zinc-200 dark:border-zinc-800 bg-zinc-950 flex flex-col gap-2 items-center p-2 rounded-lg ">
+          <Button className="w-full" variant={"ghost"}>
+            Form
+            <FormInput />
+          </Button>
+          <DropdownMenuSeparator />
+          <Button className="w-full" variant={"ghost"}>
+            Conditional
+          </Button>
+          <DropdownMenuSeparator />
+          <Button className="w-full" variant={"ghost"}>
+            Approver
+          </Button>
+          <DropdownMenuSeparator />
+          <Button className="w-full" variant={"ghost"}>
+            Webhook
+          </Button>
+        </Panel>
         <Controls
           showFitView
           color="#121212"
