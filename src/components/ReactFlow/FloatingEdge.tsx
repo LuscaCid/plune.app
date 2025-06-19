9921e0e7fdc095905a15cf6b80108a5c0bfdd712
Lucas Cid <lucasfelipaaa@gmail.com>
2025-06-18 22:44:18 -0300

    feat: add FloatingConnectionLine component and refactor edge parameter calculations for improved edge rendering

--- src/components/ReactFlow/FloatingEdge.tsx
+++ src/components/ReactFlow/FloatingEdge.tsx
@@ -1,14 +1,9 @@
-import { useCallback } from 'react';
-import { useStore, getSmoothStepPath, type EdgeProps, BaseEdge } from 'reactflow';
-import "../../lib/animations.css";
 import { getEdgeParams } from '@/lib/edgeParams';
+import { useCallback } from 'react';
+import { useStore, getBezierPath, type EdgeProps } from 'reactflow';
 
-/**
- * @author Lucas Cid <lucasfelipaaa@gmail.com>
- * @summary Tipo de edge que vai mapear os nós proximos e vai redirecionar a qual handler realizar conexao
- * @created 29/06/2024
- */
-export function FloatingEdge({ id, source, target, markerEnd, style, selected } : EdgeProps) {
+
+export function FloatingEdge({ id, source, target, markerEnd, style, selected }: EdgeProps) {
   const sourceNode = useStore(useCallback((store) => store.nodeInternals.get(source), [source]));
   const targetNode = useStore(useCallback((store) => store.nodeInternals.get(target), [target]));
 
@@ -18,7 +13,7 @@ export function FloatingEdge({ id, source, target, markerEnd, style, selected } 
 
   const { sx, sy, tx, ty, sourcePos, targetPos } = getEdgeParams(sourceNode, targetNode);
 
-  const [edgePath] = getSmoothStepPath({
+  const [edgePath] = getBezierPath({
     sourceX: sx,
     sourceY: sy,
     sourcePosition: sourcePos,
@@ -28,17 +23,13 @@ export function FloatingEdge({ id, source, target, markerEnd, style, selected } 
   });
 
   return (
-  <>
-        <BaseEdge markerEnd={markerEnd} path={edgePath} />
-        <path
-            id={id}
-            className={`react-flow__edge-path hover:stroke-[5] stroke-[4] stroke-transition  ${selected ? "stroke-orange-600 stroke-[6]" : "stroke-zinc-400"} `}
-            d={edgePath}
-            strokeWidth={5}
-            markerEnd={markerEnd}
-            style={style}
-        />
-  </>   
-    
+    <path
+      id={id}
+      className={`react-flow__edge-path hover:stroke-[5] stroke-[4] stroke-transition  ${selected ? "stroke-orange-600 stroke-[6]" : "stroke-zinc-400"} `}
+      strokeWidth={10}
+      d={edgePath}
+      markerEnd={markerEnd}
+      style={style}
+    />
   );
 }
