3771ccd5d3721394dc3b9bb2a72242ca9f74942e
Lucas Cid <lucasfelipaaa@gmail.com>
2025-07-09 23:03:34 -0300

    style: uses the dropdown from shadcn

--- src/components/UserDropdown.tsx
+++ src/components/UserDropdown.tsx
@@ -6,43 +6,41 @@ import { TypographyMuted, TypographySmall } from "./ui/Typography";
 import { useThemeStore } from "@/store/theme";
 import { useUser } from "@/hooks/use-user";
 import { useUserStore } from "@/store/user";
-import * as Dropdown from "@radix-ui/react-dropdown-menu";
-import { DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator } from "./ui/dropdown-menu";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "./ui/dropdown-menu";
 import { User } from "@/@types/user";
 export function NativeUserDropdown() {
   const user = useUserStore(state => state.user)
   const { setTheme, theme } = useThemeStore()
   const { logout } = useUser();
   return (
-    <Dropdown.Root>
-      <Dropdown.Trigger>
-        <UserSidebarButton user={user}/>
-      </Dropdown.Trigger>
-      <Dropdown.Portal>
-        <Dropdown.Content align="end" className=" text-popover-foreground bg-zinc-50  dark:bg-zinc-800 rounded-lg border  dark:border-zinc-700 p-1 z-50">
-          <Dropdown.Group className="flex flex-col ">
-            <DropdownMenuLabel >
-              Account
-            </DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <CustomDropdownMenuItem icon={SunMoon} onClick={() => setTheme(theme == "dark" ? "light" : "dark")} title="Theme" />
-            <CustomDropdownMenuItem icon={UserIcon} onClick={() => console.log("Perfil")} title={user ? user.email! : ""} />
-            <CustomDropdownMenuItem icon={LogOut} onClick={logout} title="Logout" />
-          </Dropdown.Group>
-        </Dropdown.Content>
-      </Dropdown.Portal>
-    </Dropdown.Root>
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <UserSidebarButton user={user} />
+      </DropdownMenuTrigger>
+      <CustomDropwdownMenuContent>
+        <DropdownMenuLabel >
+          Account
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <CustomDropdownMenuItem icon={SunMoon} onClick={() => setTheme(theme == "dark" ? "light" : "dark")} title="Theme" />
+        <CustomDropdownMenuItem icon={UserIcon} onClick={() => console.log("Perfil")} title={user ? user.email! : ""} />
+        <CustomDropdownMenuItem icon={LogOut} onClick={logout} title="Logout" />
+      </CustomDropwdownMenuContent>
+    </DropdownMenu>
+    
   );
 }
 interface Props {
   title: string;
   icon: LucideIcon;
   onClick: (e: MouseEvent) => void;
+  variant?: "default" | "destructive"
 }
-export function CustomDropdownMenuItem({ icon: Icon, onClick, title }: Props) {
+export function CustomDropdownMenuItem({ icon: Icon, onClick, title, variant = "default" }: Props) {
   return (
     <DropdownMenuItem
       onClick={onClick}
+      variant={variant}
       className="px-2 py-2 hover:bg-zinc-200 flex items-center gap-2 dark:hover:bg-zinc-700"
     >
       <Icon size={15} />
