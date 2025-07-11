241c0bce9a1ff08b59f7014a1efe7ced520a80bc
Lucas Cid <lucasfelipaaa@gmail.com>
2025-07-11 17:33:04 -0300

    wip: adding the flow template dialog form

--- src/pages/app/FlowTemplates.tsx
+++ src/pages/app/FlowTemplates.tsx
@@ -7,6 +7,13 @@ import { useQuery } from "@tanstack/react-query";
 import { useMemo, useState } from "react";
 import { useFlowColumns } from "@/hooks/use-flow-columns";
 import { useUserStore } from "@/store/user";
+import { Dialog, DialogContent, DialogDescription, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { Plus } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { FormItem } from "@/components/ui/form";
+import { FormProvider, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { FlowDTO, SaveFlowDTO } from "@/lib/DTO/flow.dto";
 
 export function FlowTemplates() {
   const [pageIndex, setPageIndex] = useState(1);
@@ -23,25 +30,53 @@ export function FlowTemplates() {
   ]), [selectedOrganization ? selectedOrganization.id : undefined, pageIndex]);
 
   const { getOrganizationTemplateFlows } = useFlow();
+
+  // adicionar possibilidade de edição ao clicar no item dentro da listagem
   const { templateColumns } = useFlowColumns("/flows/diagram/template/");
 
   const { data: response, isLoading } = useQuery({
-    queryFn: async () => {
-      try {
-        return await getOrganizationTemplateFlows({ page: pageIndex }) as GetFlowReturn
-      } catch (err) {
-        console.log(err)
-      }
-    },
+    queryFn: async () => await getOrganizationTemplateFlows({ page: pageIndex }) as GetFlowReturn,
     queryKey: flowQueryKey,
     refetchOnWindowFocus: false,
-    enabled : selectedOrganization != undefined
+    enabled: selectedOrganization != undefined
+  });
+
+  const methods = useForm<SaveFlowDTO>({
+    resolver: zodResolver(FlowDTO.saveFlowDTO),
+    defaultValues: {}
   });
 
   return (
     <ScreenWrapper>
       {!isLoading && response && (
         <GenericTable<Flow>
+          newItem={{
+            dialog: (
+              <Dialog>
+                <DialogTrigger>
+                  <Button variant={"outline"}>
+                    <Plus size={15} />
+                    New
+                  </Button>
+                </DialogTrigger>
+                <DialogContent>
+                  <DialogTitle>
+                    New Flow Template
+                  </DialogTitle>
+                  <DialogDescription>
+                    Create an new or update someone existing here
+                  </DialogDescription>
+                  <FormProvider {...methods}>
+                    <form action="">
+                      <FormItem>
+
+                      </FormItem>
+                    </form>
+                  </FormProvider>
+                </DialogContent>
+              </Dialog>
+            )
+          }}
           searchFilterColumnInput={{ accssorKey: "name", placelholder: "Flow name..." }}
           data={response.data}
           manualPagination={true}
